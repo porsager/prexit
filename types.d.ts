70310d8fd72234d22486c42e4bc7c569b54ee823
Gilbert <gilbertbgarza@gmail.com>
2024-07-31 12:06:27 -0500

    Update types.d.ts. Fixes #9

--- types.d.ts
+++ types.d.ts
@@ -8,18 +8,19 @@ type PrexitSignal =
   | "SIGINT";
 
 declare module "prexit" {
-  export let code: number;
-  export function ondone(): void;
-  export function last(
-    callback?: (signal: PrexitSignal, code: number, error?: Error) => void
-  ): void;
-  export function exit(signal?: PrexitSignal, code?: number): void;
-  export function exit(code: number): void;
-  export default function (
-    callback?: (signal: PrexitSignal, code: number, error?: Error) => unknown | Promise<unknown>
-  ): void;
-  export default function (
-    event: string | string[],
-    callback?: (signal: PrexitSignal, code: number, error?: Error) => unknown | Promise<unknown>
-  ): void;
+  interface Prexit {
+    code: number;
+    ondone(): void;
+    last(
+      callback?: (signal: PrexitSignal, code: number, error?: Error) => void
+    ): void;
+    exit(signal?: PrexitSignal, code?: number): void;
+    exit(code: number): void;
+    (callback?: (signal: PrexitSignal, code: number, error?: Error) => unknown | Promise<unknown>): void;
+    (event: string | string[], callback?: (signal: PrexitSignal, code: number, error?: Error) => unknown | Promise<unknown>): void;
+  }
+
+  const prexit: Prexit;
+
+  export default prexit;
 }
